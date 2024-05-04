cb58ea0af86d6eb03069768518c551a03e58253c
A V REVANTH <105591606+ayitharevanth@users.noreply.github.com>
2024-05-04 13:35:45 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://eshopreva-revanthayitha2021-gmailcoms-projects.vercel.app/api/v2";
+export const server = "https://eshop-tutorial.onrender.com/api/v2";
 
 export const backend_url = "https://eshop-tutorial.onrender.com/";
 
